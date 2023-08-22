ffe09d3d32f6ec848ffd875800a6788d59618cf8
Gracevo <127609745+Gracevo@users.noreply.github.com>
2023-08-21 20:00:45 -0500

    first commit

---
+++ src/test/java/com/juanita/juanitademo/JuanitademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanita.juanitademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanitademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
